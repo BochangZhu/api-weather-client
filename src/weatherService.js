5317b771906e54e907b81b0faf379e7379a20e18
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2026-09-09 16:59:32 -0700

    feat: weatherService module with fetch-helper functions

---
+++ src/weatherService.js
@@ -0,0 +1,26 @@
+export class weatherService {  
+    #api;
+    baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+    
+    constructor(apiKey){
+        this.#api = apiKey;
+    }
+
+    // fetch raw weather object
+    async fetchWeather(location) {
+        const currURL = `${this.baseURL + location}?key=${this.#api}&include=current`;
+        const response = await fetch(currURL);
+        if (!response.ok) throw new Error(`API Request fails with status: ${response.status}`);
+        return response.json();
+    }
+
+    // extract essential weather info
+    async filterWeather(objPromise) {
+        const weatherObj = await objPromise;
+        const currCondition = weatherObj["currentConditions"];
+        const {resolvedAddress: addr} = weatherObj;
+        const {datetime, temp, feelslike, conditions, icon: iconDes} = currCondition;
+        return {addr, datetime, temp, feelslike, conditions, iconDes};
+    }
+
+}
